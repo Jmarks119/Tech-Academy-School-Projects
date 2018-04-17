cf5744d5850e36101e367d10bcb5c601d69d558d
Jackson Marks <Jmarks119@gmail.com>
2018-04-16 19:53:54 -0700

    Add initial C# demo projects.

---
+++ FirstCSharpApp/FirstCSharpApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCSharpApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FirstCSharpApp/Variables/Variables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.Read();
+            Console.WriteLine("What is your favorite number?");
+            string favoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(favoriteNumber);
+            int total = favNum + 5;
+            Console.WriteLine("Your favorite number is: " + favoriteNumber);
+            Console.WriteLine("Your favorite number + 5 is: " + total);
+            Console.ReadLine();
+        }
+    }
+}
