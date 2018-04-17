e1a3d33094e1020031a198ede91f38d67542d38a
Jackson Marks <Jmarks119@gmail.com>
2018-04-17 16:25:06 -0700

    Add branching practice program.

---
+++ FirstCSharpApp/Branching/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main()
+        {
+            int roomTemperature = 70;
+
+            Console.WriteLine("Hi, what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            if (currentTemperature == roomTemperature)
+            {
+                Console.WriteLine("It is exactly room temperature.");
+            }
+            else if (currentTemperature > roomTemperature)
+            {
+                Console.WriteLine("It is warmer than room temperature.");
+            }
+            else
+            {
+                Console.WriteLine("It is colder than room temperature.");
+            }
+            Console.ReadLine();
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temperature" : "It is not room temperature.";
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ FirstCSharpApp/Math/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 5 + 10;
+            Console.WriteLine("Five plus Ten = " + total.ToString());
+            Console.ReadLine();
+            int othertotal = 12 + 22;
+            int combined = total + othertotal;
+            Console.WriteLine("That, plus another number is: " + combined.ToString());
+            Console.ReadLine();
+
+            int difference = 12 - 5;
+            Console.WriteLine("Twelve minus five is: " + difference.ToString());
+            Console.ReadLine();
+
+            int product = 20 * 4;
+            Console.WriteLine("Twenty times Four is: " + product.ToString());
+            Console.ReadLine();
+
+            double quotient = 60.0 / 7.0;
+            Console.WriteLine("Sixty divided by Seven is: " + quotient.ToString());
+            Console.ReadLine();
+
+            int remainder = 23 % 4;
+            Console.WriteLine("The remainder of 23 divided by 4 is: " + remainder.ToString());
+            Console.ReadLine();
+
+            bool trueOrFalse = 12 < 5;
+            Console.WriteLine("Is 12 less than 5?: " + trueOrFalse.ToString());
+            Console.ReadLine();
+        }
+    }
+}
