dadfc211881003cfceced5ae7a951c81574d26ea
Jackson Marks <Jmarks119@gmail.com>
2018-04-17 16:22:28 -0700

    Add first Csharp drills.

---
+++ CSharpDrill1/CSharpDrill1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpDrill1._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Is 3 greater than 2 AND 5 greater than 4?");
+            Console.WriteLine(Convert.ToString(3 > 2 && 5 > 4));
+            Console.WriteLine("Is 3 greater than 2 OR 5 greater than 4?");
+            Console.WriteLine(Convert.ToString(3 > 2 || 5 > 4));
+            Console.WriteLine("Is 2 + 2 NOT EQUAL to 4?");
+            Console.WriteLine(Convert.ToString(2 + 2 != 4));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpDrill1/CSharpDrill1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace CSharpDrill1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Please Type in an integer: ");
+            try
+            {
+                int multiplicationInput = Convert.ToInt32(Console.ReadLine());
+                int multiplicationResult = multiplicationInput * 50;
+                Console.WriteLine(Convert.ToString(multiplicationInput) + " multiplied by 50 is: " + Convert.ToString(multiplicationResult));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only type characters 0-9");
+            }
+            Console.ReadLine();
+
+            Console.Write("Please Type in an integer: ");
+            try
+            {
+                int additionInput = Convert.ToInt32(Console.ReadLine());
+                int additionResult = additionInput + 25;
+                Console.WriteLine(Convert.ToString(additionInput) + " added to 25 is: " + Convert.ToString(additionResult));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only type characters 0-9");
+            }
+            Console.ReadLine();
+
+            Console.Write("Please Type in a number: ");
+            try
+            {
+                float divisionInput = Convert.ToSingle(Console.ReadLine());
+                float divisionResult = divisionInput / 12.5f;
+                Console.WriteLine(Convert.ToString(divisionInput) + " divided by 12.5 is: " + Convert.ToString(divisionResult));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only type characters 0-9 and .");
+            }
+            Console.ReadLine();
+
+            Console.Write("Please Type in an integer: ");
+            try
+            {
+                int booleanInput = Convert.ToInt32(Console.ReadLine());
+                if (booleanInput > 50)
+                {
+                    Console.WriteLine(Convert.ToString(booleanInput) + " is greater than 50.");
+                }
+                else
+                {
+                    Console.WriteLine(Convert.ToString(booleanInput) + " is not greater than 50.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only type characters 0-9");
+            }
+            Console.ReadLine();
+
+            Console.Write("Please Type in an integer: ");
+            try
+            {
+                int moduloInput = Convert.ToInt32(Console.ReadLine());
+                int moduloResult = moduloInput % 7;
+                Console.WriteLine("The remainder of " + Convert.ToString(moduloInput) + " divided by 7 is: " + Convert.ToString(moduloResult));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only type characters 0-9");
+            }
+            Console.ReadLine();
+        }
+    }
+}
