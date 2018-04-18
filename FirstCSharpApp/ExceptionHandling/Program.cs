d20680c4194e2d50612eb5737184c905f49bc59f
Jackson Marks <Jmarks119@gmail.com>
2018-04-18 15:31:17 -0700

    Add Exception Handling practice program.

---
+++ FirstCSharpApp/ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int NumberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / NumberTwo;
+                Console.WriteLine(numberOne + " divided by " + NumberTwo + " equals " + numberThree);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please type a whole number");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please don't divide by 0");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
