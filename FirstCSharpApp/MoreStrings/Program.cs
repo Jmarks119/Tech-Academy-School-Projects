0aedcdcdc4c60f94ec1b965a438651b11e6a64ce
Jackson Marks <Jmarks119@gmail.com>
2018-04-17 18:37:14 -0700

    Add string practice program.

---
+++ FirstCSharpApp/MoreStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoreStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "Jackson";
+            string quote = "The man said, \"Hello\", Jackson. \n Hello on a new line. \n \t Hello on a tab.";
+            string fileName = @"C:\Users\Jackson";
+
+            bool contains = name.Contains("k");
+            int length = name.Length;
+            string lowerName = name.ToLower();
+
+            Console.WriteLine(quote);
+            Console.ReadLine();
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            sb.Append("My name is Jackson.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
