0239fa809ad216180f59fab28184d6ecadb3e4d2
Jackson Marks <Jmarks119@gmail.com>
2018-04-17 18:22:30 -0700

    Add CSharp Drill 3 (While loops)

---
+++ CSharpDrill3/CSharpDrill3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharpDrill3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = 23;
+            while (number > 0)
+            {
+                Console.WriteLine(Convert.ToString(number));
+                number -= 1;
+            }
+            Console.ReadLine();
+
+            int inputNumber = 0;
+            do
+            {
+                Console.WriteLine("You're trapped in a loop! Type in the correct number to escape:");
+                inputNumber = Convert.ToInt32(Console.ReadLine());
+            } while (inputNumber != 23);
+        }
+    }
+}
