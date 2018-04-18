de70bceb08dc63e5393d346c1f7dd7bc56606050
Jackson Marks <Jmarks119@gmail.com>
2018-04-17 17:17:00 -0700

    Add second CSharp drill (conditional branching)

---
+++ CSharpDrill2/CSharpDrill2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpDrill2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("What is your latitude purely in degrees? ");
+            float userLatitude = Convert.ToSingle(Console.ReadLine());
+            if (userLatitude < 23.43691f)
+            {
+                Console.WriteLine("You live in the tropics!");
+            }
+            else if (userLatitude > 66.57f)
+            {
+                Console.WriteLine("You live inside of the arctic or antarctic circle!");
+            }
+            string equatorResult = userLatitude == 0f ? "You live at the equator!" : "You don't live at the equator...";
+            Console.WriteLine(equatorResult);
+            Console.ReadLine();
+        }
+    }
+}
