36d54abac18891822796d1ca8c578bdc90312373
Jackson Marks <Jmarks119@gmail.com>
2018-04-19 16:29:17 -0700

    Add C# drill 11 (Void methods and named paramaters)

---
+++ CSharpDrill11/CSharpDrill11/HiddenMathVoidedParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpDrill11
+{
+    class HiddenMathVoidedParams
+    {
+        public void TrashCompactor(int eatenInteger, int displayedInteger)
+        {
+            eatenInteger *= 0;
+            Console.WriteLine("Your first paramater has been obliterated. The second paramater was: " + displayedInteger);
+        }
+    }
+}
---
+++ CSharpDrill11/CSharpDrill11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill11
+{
+    class Program
+    {
+        static void Main()
+        {
+            HiddenMathVoidedParams mathObject = new HiddenMathVoidedParams();
+            mathObject.TrashCompactor(69, 42);
+            mathObject.TrashCompactor(displayedInteger: 119, eatenInteger: 55);
+            Console.ReadLine();
+        }
+    }
+}
