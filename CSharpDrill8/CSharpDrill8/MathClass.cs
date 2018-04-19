7f5c560f044c93654c74b92d6359b0208f7cea9e
Jackson Marks <Jmarks119@gmail.com>
2018-04-19 14:35:50 -0700

    Add C# Drill 8 (Methods)

---
+++ CSharpDrill8/CSharpDrill8/MathClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill8
+{
+    class MathClass
+    {
+        public static int SomeAddition(int number)
+        {
+            int newNumber = number + 5;
+            return newNumber;
+        }
+        public static int SomeSubtraction(int number)
+        {
+            int newNumber = number - 11;
+            return newNumber;
+        }
+        public static int SomeMultiplication(int number)
+        {
+            int newNumber = number * 21;
+            return newNumber;
+        }
+    }
+}
---
+++ CSharpDrill8/CSharpDrill8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpDrill8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, let's do a little math!");
+            try
+            {
+                Console.Write("Please type in a whole number: ");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Your number + 5 is: " + MathClass.SomeAddition(userInput));
+                Console.WriteLine("Your number - 11 is: " + MathClass.SomeSubtraction(userInput));
+                Console.WriteLine("Your number * 21 is " + MathClass.SomeMultiplication(userInput));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please only type in a whole number.");
+            }
+            finally
+            {
+                Console.WriteLine("Have a nice day!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
