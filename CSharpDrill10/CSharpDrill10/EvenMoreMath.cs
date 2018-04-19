8d92e6a3fa9e058f109a673183d954c9a78fea36
Jackson Marks <Jmarks119@gmail.com>
2018-04-19 15:59:14 -0700

    Added C# Drill 10 (optional parameters)

---
+++ CSharpDrill10/CSharpDrill10/EvenMoreMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill10
+{
+    class EvenMoreMath
+    {
+        public int SometimesMultipleParams(int input1, int input2 = 42)
+        {
+            int newNumber = input1 * input2;
+            return newNumber;
+        }
+    }
+}
---
+++ CSharpDrill10/CSharpDrill10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill10
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Let's do even more silly math!");
+            Console.Write("Type in a whole number: ");
+            int input1 = 0;
+            try
+            {
+                input1 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please only input a whole number");
+            }
+            Console.Write("Type in another whole number, or just hit enter without typing anything: ");
+            string rawInput2 = Console.ReadLine();
+            EvenMoreMath mathObject = new EvenMoreMath();
+            try
+            {
+                if (rawInput2 == "")
+                {
+                    Console.WriteLine("Your first integer times my secret integer is: " + mathObject.SometimesMultipleParams(input1));
+                }
+                else
+                {
+                    Console.WriteLine("Your first integer times your second integer is: " + mathObject.SometimesMultipleParams(input1, Convert.ToInt32(rawInput2)));
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please type in only either a whole number, or nothing.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
