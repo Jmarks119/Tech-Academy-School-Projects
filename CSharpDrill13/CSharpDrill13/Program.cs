02390f6f1a2551a3c709cbbe8046c0670aa8be6f
Jackson Marks <Jmarks119@gmail.com>
2018-04-19 17:48:13 -0700

    Add C# Drill 13 (Inheritance)

---
+++ CSharpDrill13/CSharpDrill13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sampleEmployee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            sampleEmployee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
