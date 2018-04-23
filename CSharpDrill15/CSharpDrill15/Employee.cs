7868386895faf0cec5fa3c9e36321a57f614aaab
Jackson Marks <Jmarks119@gmail.com>
2018-04-23 14:19:54 -0700

    Add C# Drill 15 (interface components)

---
+++ CSharpDrill15/CSharpDrill15/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill15
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: [{0} {1}]", FirstName, LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Exiting now...");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ CSharpDrill15/CSharpDrill15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill15
+{
+    class Program
+    {
+        static void Main()
+        {
+            IQuittable quittable = new Employee();
+            quittable.Quit();
+        }
+    }
+}
