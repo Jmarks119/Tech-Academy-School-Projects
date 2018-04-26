53647c489b9483e0f19401afd033d06148e4c085
Jackson Marks <Jmarks119@gmail.com>
2018-04-26 14:57:48 -0700

    Add C# drill 17 (enums)

---
+++ CSharpDrill17/CSharpDrill17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpDrill17
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Please type in the current day of the week: ");
+                DayOfTheWeek day = (DayOfTheWeek)Enum.Parse(typeof(DayOfTheWeek), Console.ReadLine());
+                Console.WriteLine("You put in " + day);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please provide a correct day of the week. (Capitalize correctly)");
+            }
+            Console.ReadLine();
+        }
+        public enum DayOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
