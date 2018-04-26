43942178312df16f617aa44d8e291ea2a0973d33
Jackson Marks <Jmarks119@gmail.com>
2018-04-26 15:45:00 -0700

    Add C# Drill 18 (structs)

---
+++ CSharpDrill18/CSharpDrill18/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill18
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number number = new Number();
+            number.Amount = 1209312098.0918209825m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
