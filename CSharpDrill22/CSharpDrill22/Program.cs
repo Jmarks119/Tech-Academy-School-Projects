4a7cfca125d5caf39dc2a5a4b9beafe1a8ae23d4
Jackson Marks <Jmarks119@gmail.com>
2018-04-30 16:29:36 -0700

    Add C# Drill 22 (constants, constructor chaining, var)

---
+++ CSharpDrill22/CSharpDrill22/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill22
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string soloMessage = "This is a message";
+            const string messageTwoPart1 = "And this is a message";
+            const string messageTwoPart2 = " built from two constants";
+
+            var messageDisplayOne = new MessageBuilder(soloMessage);
+            var messageDisplayTwo = new MessageBuilder(messageTwoPart1, messageTwoPart2);
+
+            messageDisplayOne.DisplayMessage();
+            messageDisplayTwo.DisplayMessage();
+            Console.ReadLine();
+        }
+    }
+    class MessageBuilder
+    {
+        public MessageBuilder(string oneMessage) : this(oneMessage, "") { }
+        public MessageBuilder(string oneMessage, string secondMessage)
+        {
+            FullMessage = oneMessage + secondMessage;
+        }
+
+        public string FullMessage { get; set; }
+
+        public void DisplayMessage()
+        {
+            Console.WriteLine(FullMessage);
+        }
+    }
+}
