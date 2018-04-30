5ef1c4c2686da8361a14ed0a20921c6b3a946382
Jackson Marks <Jmarks119@gmail.com>
2018-04-30 11:50:54 -0700

    Add C# Drill 20 (StreamReader/Writer)

---
+++ CSharpDrill20/CSharpDrill20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace CSharpDrill20
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamWriter logWriter = new StreamWriter(@"C:\Users\Jmark\Documents\Coding Projects\Sample Logs\log.txt", true);
+            Console.Write("Please type in a number: ");
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                logWriter.WriteLine(input + "\n");
+                logWriter.Close();
+                StreamReader logReader = new StreamReader(@"C:\Users\Jmark\Documents\Coding Projects\Sample Logs\log.txt");
+                Console.WriteLine("Here's the whole log file so far:");
+                Console.Write(logReader.ReadToEnd());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please only type in a whole number!");
+            }
+            Console.Read();
+        }
+    }
+}
