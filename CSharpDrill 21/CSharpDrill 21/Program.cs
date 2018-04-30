f1377ef42e532d08d9627211fa490e4282edd1d3
Jackson Marks <Jmarks119@gmail.com>
2018-04-30 12:25:25 -0700

    Add C# Drill 21 (Datetime objects)

---
+++ CSharpDrill 21/CSharpDrill 21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrill_21
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("It is currently " + DateTime.Now);
+            Console.Write("Please type in a number: ");
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                TimeSpan timeSpan = new TimeSpan(input, 0, 0);
+                Console.WriteLine("In " + input + " hours it will be " + (DateTime.Now + timeSpan));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please only type in a whole number!");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ CSharpDrill 21/CSharpDrill 21/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharpDrill 21")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpDrill 21")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7dc210d6-dea9-41e4-91e2-a8830b140262")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
