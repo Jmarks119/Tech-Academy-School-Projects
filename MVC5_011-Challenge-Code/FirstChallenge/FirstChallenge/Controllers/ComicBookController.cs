39fd786fd6b962addc7649cb8deed9627eb28182
Jackson <Jmarks119@gmail.com>
2018-05-17 18:40:37 -0700

    Finished webapp according to desired look and function.

---
+++ MVC5_011-Challenge-Code/FirstChallenge/FirstChallenge/Controllers/ComicBookController.cs
@@ -0,0 +1,42 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        private static List<ComicBook> _comicBooks = ComicBookManager.GetComicBooks();
+
+        // GET: ComicBook
+        public ActionResult Index()
+        {
+            return View(_comicBooks);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            ComicBook comicBookToReturn = null;
+            foreach (var comicBook in _comicBooks)
+            {
+                if (comicBook.ComicBookId == id)
+                {
+                    comicBookToReturn = comicBook;
+                    break;
+                }
+            }
+            if (comicBookToReturn == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comicBookToReturn);
+        }
+    }
+}
